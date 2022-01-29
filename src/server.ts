881c38a57d64491d003367d819282e8f1218836f
Nathan Holm <nathan@wholm.com>
2022-01-29 04:09:45 -0800

    Refactor server.ts  with Apollo Server
    - Remove mercurius references
    - Add server instantiation for Apollo Server

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,54 @@
 // Kibbel API Server ----------------------------------------------------------
-import fastify, {
-  FastifyError,
-  FastifyServerOptions,
-  FastifyInstance,
-} from 'fastify';
-import mercurius from 'mercurius';
+import 'reflect-metadata';
+import fastify, { FastifyInstance, FastifyServerOptions } from 'fastify';
+import { ApolloServer } from 'apollo-server-fastify';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServerPlugin } from 'apollo-server-plugin-base';
 import chalk from 'chalk';
-import schema from './schema';
-import { context } from './context';
+import { buildSchema } from 'type-graphql';
+import { UserResolver } from './resolvers';
+import database from './plugins/database';
 
-const createServer = (options: FastifyServerOptions = {}): FastifyInstance => {
-  const server = fastify(options);
+const fastifyAppClosePlugin = (app: FastifyInstance): ApolloServerPlugin => {
+  return {
+    async serverWillStart() {
+      return {
+        async drainServer() {
+          await app.close();
+        },
+      };
+    },
+  };
+};
 
-  server.register(mercurius, { schema, graphiql: true });
+const createServer = async (options: FastifyServerOptions = {}) => {
+  const app = fastify(options);
 
-  return server;
+  const schema = await buildSchema({ resolvers: [UserResolver] });
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      fastifyAppClosePlugin(app),
+      ApolloServerPluginDrainHttpServer({ httpServer: app.server }),
+    ],
+  });
+
+  await server.start();
+  app.register(server.createHandler());
+  app.register(database);
+  const SERVER_PORT = process.env.SERVER_PORT
+    ? parseInt(process.env.SERVER_PORT)
+    : 3000;
+  app.listen(SERVER_PORT);
 };
 
 const startServer = async () => {
-  const server = createServer({ logger: true });
   try {
-    const SERVER_PORT = process.env.SERVER_PORT
-      ? parseInt(process.env.SERVER_PORT)
-      : 3000;
-    await server.listen(SERVER_PORT);
+    await createServer({ logger: true });
     console.log(chalk.hex('#27CECE')('Kibbel Server'));
-    console.log(`Running on port ${SERVER_PORT}`);
   } catch (error) {
-    console.log(chalk.red('Kibbel Server'));
-    server.log.error(error);
+    console.error(chalk.red('Kibbel Server'));
+    console.error(error);
     process.exit(1);
   }
 };
