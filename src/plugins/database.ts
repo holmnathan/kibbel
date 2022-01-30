00a4b83a84561213b054152301bff8ed8345b865
Nathan Holm <nathan@wholm.com>
2022-01-29 17:10:00 -0800

    Refactor entity imports
    - Add Diet model

--- src/plugins/database.ts
+++ src/plugins/database.ts
@@ -3,20 +3,23 @@ import fastifyPlugin from 'fastify-plugin';
 import { createConnection, getConnectionOptions } from 'typeorm';
 import * as entities from '../models';
 
+const { User, Pet, Diet } = entities;
+
 const database = fastifyPlugin(async (server) => {
   try {
     // getConnectionOptions will read from environment variables
     const connectionOptions = await getConnectionOptions();
     Object.assign(connectionOptions, {
       options: { encrypt: true },
-      entities: [entities.User, entities.Pet],
+      entities: [User, Pet, Diet],
     });
     const connection = await createConnection(connectionOptions);
 
     // this object will be accessible from any fastify server instance
     server.decorate('database', {
-      user: connection.getRepository(entities.User),
-      pet: connection.getRepository(entities.Pet),
+      user: connection.getRepository(User),
+      pet: connection.getRepository(Pet),
+      diet: connection.getRepository(Diet),
     });
   } catch (error) {
     console.log(error);
