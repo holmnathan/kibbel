8cb69e8149515e4b087b9cc6ad4244370ecab015
Nathan Holm <nathan@wholm.com>
2022-01-29 17:08:10 -0800

    Add TypeORM comments
    - Add default export
    - Add export enumerated types
    - Add "Many to One" relationship with Diet model

--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -5,10 +5,12 @@ import {
   Column,
   CreateDateColumn,
   OneToOne,
+  ManyToOne,
   JoinColumn,
 } from 'typeorm';
 import { ObjectType, Field, ID, registerEnumType } from 'type-graphql';
-import { User } from './User';
+import User from './User';
+import Diet from './Diet';
 
 // Enumerated Types -----------------------------------------------------------
 // Each Type is defined and registered with TypeGraphQL
@@ -41,57 +43,79 @@ registerEnumType(PetGender, {
 // TypeOrm decorators:     @Entity, @[*]Column, @[*]To[*]
 // TypeGraphQL decorators: @ObjectType, @Field
 
+// Shared TypeGraphQL descriptions and TypeORM comments
+const sharedComments = {
+  name: 'The pet’s name',
+  birthDate: 'The pet’s date of birth',
+  imageUrl: 'URL of pet’s uploaded profile image',
+  isIntact: 'The pet’s reproductive status',
+  species: 'The pet’s species',
+  gender: 'The pet’s gender',
+  createdAt: 'The pet’s profile creation date',
+};
+
 @Entity()
 @ObjectType({ description: 'Pet Schema' })
-export class Pet extends BaseEntity {
+class Pet extends BaseEntity {
   @Field(() => ID)
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
   @Field({
-    description: 'Th pet’s name',
+    description: sharedComments.name,
   })
-  @Column()
+  @Column({ comment: sharedComments.name })
   name!: string;
 
   @Field({
-    description: 'The pet’s date of birth',
+    description: sharedComments.birthDate,
   })
-  @Column()
+  @Column({ comment: sharedComments.birthDate })
   birthDate!: Date;
 
   @Field({
     nullable: true,
-    description: 'URL of pet’s uploaded profile image',
+    description: sharedComments.imageUrl,
   })
-  @Column({ nullable: true })
+  @Column({ comment: sharedComments.imageUrl, nullable: true })
   imageUrl?: string;
 
   @Field({
-    description: 'The pet’s reproductive status',
+    description: sharedComments.isIntact,
   })
-  @Column()
+  @Column({ comment: sharedComments.isIntact })
   isIntact!: boolean;
 
   @Field(() => PetSpecies, {
-    description: 'The pet’s species',
+    description: sharedComments.species,
   })
-  @Column({ type: 'enum', enum: PetSpecies })
+  @Column({ comment: sharedComments.species, type: 'enum', enum: PetSpecies })
   species!: PetSpecies;
 
   @Field(() => PetGender, {
     nullable: true,
-    description: 'The pet’s gender',
+    description: sharedComments.gender,
   })
-  @Column({ nullable: true, type: 'enum', enum: PetGender })
+  @Column({
+    comment: sharedComments.gender,
+    nullable: true,
+    type: 'enum',
+    enum: PetGender,
+  })
   gender?: PetGender;
 
-  @Field({ description: 'The pet’s profile creation date' })
-  @CreateDateColumn()
+  @Field({ description: sharedComments.createdAt })
+  @CreateDateColumn({ comment: sharedComments.createdAt })
   createdAt!: Date;
 
   // No @Field() specified: Hides field from public access
   @OneToOne(() => User, (user) => user.pet)
   @JoinColumn()
   user!: User;
+
+  @ManyToOne(() => Diet)
+  diet!: Diet;
 }
+
+export default Pet;
+export { Pet, PetGender, PetSpecies };
