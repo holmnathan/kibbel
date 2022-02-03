60816ef87824b39067367c051cc610812714664a
Nathan Holm <nathan@wholm.com>
2022-02-03 14:27:39 -0800

    Add User registration and login resolvers
    - Add User password hashing
    - Add return a JSON web token (JWT) on successfull login

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,6 +1,8 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
-import { User } from '../entities/';
-import { CreateUserInput, UpdateUserInput } from '../inputs';
+import { Resolver, Query, Mutation, Arg, Authorized } from 'type-graphql';
+import { User, LoginResponse } from '../entities/';
+import { CreateUserInput, UpdateUserInput, LogInUserInput } from '../inputs';
+import { hash, compare } from 'bcrypt';
+import { sign as jwtSign } from 'jsonwebtoken';
 
 @Resolver(User)
 class UserResolver {
@@ -14,14 +16,58 @@ class UserResolver {
     return User.find();
   }
 
-  @Mutation(() => User)
-  async createUser(@Arg('data') data: CreateUserInput) {
-    const user = User.create(data);
-    await user.save();
-    return user;
+  @Mutation(() => LoginResponse)
+  async logInUser(
+    @Arg('credentials') { email, password }: LogInUserInput
+  ): Promise<LoginResponse> {
+    const existingUser = await User.findOne({ where: { email } });
+    const { JWT_SECRET } = process.env;
+    let isValidPassword: boolean;
+    let token: string;
+
+    if (typeof JWT_SECRET !== 'string') {
+      console.log('JWT_SECRET environment variable is undefined');
+      throw new Error('Server Error');
+    }
+
+    if (!existingUser) throw new Error('Incorrect email address or password');
+
+    isValidPassword = await compare(password, existingUser.password);
+
+    if (!isValidPassword) {
+      throw new Error('Incorrect email address or password');
+    }
+
+    token = jwtSign(
+      { userId: existingUser.id, email: existingUser.email },
+      JWT_SECRET,
+      { expiresIn: 900 }
+    );
+    return { token };
   }
 
   @Mutation(() => User)
+  async createUser(
+    @Arg('data') { email, password }: CreateUserInput,
+    @Arg('data') data: CreateUserInput
+  ): Promise<User | false> {
+    try {
+      const existingUser = await User.findOne({ where: { email } });
+      if (existingUser) throw new Error('User account already exists');
+
+      const user = User.create(data);
+      user.password = await hash(password, 10);
+
+      await user.save();
+      return user;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  @Mutation(() => User)
+  @Authorized()
   async updateUser(@Arg('id') id: string, @Arg('data') data: UpdateUserInput) {
     try {
       const user = await User.findOne({ where: { id } });
@@ -32,7 +78,7 @@ class UserResolver {
       await user.save();
       return user;
     } catch (error) {
-      console.log(error);
+      return error;
     }
   }
 
