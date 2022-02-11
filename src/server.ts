65034f5e54a07ff2ec8c444036af90eb017f6051
Nathan Holm <nathan@wholm.com>
2022-02-10 17:41:40 -0800

    Add refresh user token route

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,20 @@ import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import chalk from 'chalk';
 import { buildSchema } from 'type-graphql';
 import { UserResolver } from './resolvers';
-import database from './plugins/database';
-import fastifyAppClosePlugin from './plugins/AppClose';
+import { database, appClose } from './plugins';
 import fastifyCookie, { FastifyCookieOptions } from 'fastify-cookie';
-import { authChecker } from './auth';
+import {
+  authChecker,
+  verifyRefreshToken,
+  createAccessToken,
+  createRefreshToken,
+} from './auth';
+import { User } from './entities';
 
 // Import environment variables
 
 const PORT = process.env.PORT ?? 3000;
-const { NODE_ENV, COOKIE_SECRET } = process.env;
+const { NODE_ENV, REFRESH_TOKEN_SECRET } = process.env;
 
 const createServer = async (options: FastifyServerOptions = {}) => {
   const app = fastify(options);
@@ -26,21 +31,43 @@ const createServer = async (options: FastifyServerOptions = {}) => {
   const server = new ApolloServer({
     schema,
     plugins: [
-      fastifyAppClosePlugin(app),
+      appClose(app),
       ApolloServerPluginDrainHttpServer({ httpServer: app.server }),
     ],
     context: ({ request, reply }) => ({ request, reply }),
+    introspection: NODE_ENV !== 'production',
   });
 
   await server.start();
   app.register(server.createHandler());
   app.register(database);
   app.register(fastifyCookie, {
-    secret: COOKIE_SECRET,
+    secret: REFRESH_TOKEN_SECRET,
   } as FastifyCookieOptions);
 
-  app.listen(PORT, (error, appUrl) => {
+  app.listen(PORT, (error) => {
     if (error) app.log.error(error);
+  });
+
+  app.post('/refresh-token', async (request, reply) => {
+    const refreshToken = request.cookies.kibbel;
+    if (!refreshToken) {
+      app.log.warn('Refresh Token not found');
+      return { ok: false, accessToken: null };
+    }
+    const payload = verifyRefreshToken(refreshToken);
+    if (!payload) {
+      app.log.warn('Refresh Token not valid');
+      return { ok: false, accessToken: null };
+    }
+
+    const user = await User.findOne({ id: payload.id });
+    if (!user) {
+      app.log.warn('Refresh Token not valid');
+      return { ok: false, accessToken: null };
+    }
+    reply.cookie('kibbel', createRefreshToken(user), { httpOnly: true });
+    return { ok: true, accessToken: createAccessToken(user) };
   });
 };
 
