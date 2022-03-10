dc79af578d885f426904545fd1243cd467b1b82c
Nathan Holm <nathan@wholm.com>
2022-03-10 15:52:26 -0800

    Refactor AuthLink
    - Add error if AuthLink is set as the terminating link
    - Send access token with GraphQL requests

--- web/library/apollo/AuthLink.ts
+++ web/library/apollo/AuthLink.ts
@@ -1,28 +1,35 @@
 import { ApolloLink, Observable } from "@apollo/client";
+import type { FetchResult } from "@apollo/client";
+import { AccessToken } from "../AccessToken";
+import { accessToken } from ".";
 
-import type {
-  ApolloClient,
-  Operation,
-  NextLink,
-  NormalizedCacheObject,
-} from "@apollo/client";
-
-import { getToken } from "../auth";
+import type { Operation, NextLink } from "@apollo/client";
 
 class AuthLink extends ApolloLink {
-  setAuthHeader = (operation: Operation): void => {
-    const token = getToken();
+  public setAuthHeader = (
+    operation: Operation,
+    accessToken: AccessToken
+  ): void => {
+    const { token } = accessToken;
     if (token) {
-      operation.setContext({ headers: { authorization: token } });
+      operation.setContext({ headers: { Authorization: token } });
     }
   };
 
-  request(operation: Operation, forward: NextLink) {
+  public request(
+    operation: Operation,
+    forward: NextLink
+  ): Observable<FetchResult> | null {
+    if (typeof forward !== "function") {
+      throw new Error(
+        "[Auth Link]: Auth Link is a non-terminating link and should not be the last in the composed chain"
+      );
+    }
     // Set authorization token in request header
-    this.setAuthHeader(operation);
+    console.log(accessToken.token);
+    this.setAuthHeader(operation, accessToken);
 
-    const observable = forward(operation);
-    return observable;
+    return forward(operation);
   }
 }
 
