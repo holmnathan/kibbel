18fb84fabe01f86e4e3c8d49d3a9b9f45ae04450
Nathan Holm <nathan@wholm.com>
2022-03-10 15:43:03 -0800

    Add basic user sign-up page

--- web/pages/signup.tsx
+++ web/pages/signup.tsx
@@ -1,9 +1,75 @@
-import { NextPage } from "next";
+import { Formik, Field, Form, FormikHelpers } from "formik";
+import { CreateUserInput, useCreateUserMutation } from "../graphql/generated";
+import InputField from "../components/InputField";
+import { Button, Spinner } from "react-bootstrap";
 
-const SignUp = () => {
+const SignUp: NextPage = () => {
+  const [createUser, { data, error, loading }] = useCreateUserMutation({
+    onError: (error) => {
+      console.error(error.graphQLErrors[0].extensions);
+    },
+  });
   return (
     <>
-      <h1>Create Account</h1>
+      <h1>Create Your Kibbel Account</h1>
+      <Formik
+        initialValues={{
+          fullName: "",
+          email: "",
+          displayName: "",
+          password: "",
+        }}
+        onSubmit={(values) => {
+          createUser({ variables: values });
+        }}
+      >
+        {({ values, handleChange, isSubmitting }) => (
+          <Form noValidate>
+            <InputField
+              name="fullName"
+              label="Full Name"
+              placeholder="John Smith"
+              required
+            />
+            <InputField
+              name="displayName"
+              label="Display Name"
+              placeholder="John"
+            />
+            <InputField
+              name="email"
+              label="Email"
+              placeholder="john@acme.com"
+              type="email"
+              required
+            />
+            <InputField
+              name="password"
+              label="Password"
+              type="password"
+              required
+            />
+            <Button type="submit" variant="primary" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                    className="me-2"
+                  />
+                  <span className="visually-hidden">Loading…</span>
+                </>
+              ) : (
+                <></>
+              )}
+              Continue
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 };
