41fdd960a0b08add5e3fa6d2ab16daf1e2722a5e
Nathan Holm <nathan@wholm.com>
2022-03-23 21:09:44 -0700

    Refactor Git project as workspaces
    - rename “server” folder to “api”
    - rename web “client” folder to “clients/web/”
    - refactor API and web client as NPM workspaces
    - Add “shared” NPM workspace for shared type definitions
    - Increase strictness of TypeScript “.tsconfig”

--- api/src/environment.d.ts
+++ api/src/environment.d.ts
@@ -7,6 +7,17 @@ declare global {
       ACCESS_TOKEN_SECRET: string;
       REFRESH_TOKEN_SECRET: string;
       CORS_ORIGIN_CLIENTS: string; // A JSON array of strings, must be split by "," when used.
+      TYPEORM_CONNECTION: string;
+      TYPEORM_HOST: string;
+      TYPEORM_PORT: number;
+      TYPEORM_USERNAME: string;
+      TYPEORM_PASSWORD: string;
+      TYPEORM_DATABASE: string;
+      TYPEORM_LOGGING: boolean;
+      TYPEORM_SYNCHRONIZE: boolean;
+      TYPEORM_MIGRATIONS_DIR: string;
+      TYPEORM_SUBSCRIBERS_DIR: string;
+      TYPEORM_ENTITIES_DIR: string;
     }
   }
 }
