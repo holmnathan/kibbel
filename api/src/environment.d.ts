81bf9375a858ce00dbc4f9d8adbfe3f922e4acb5
Nathan Holm <nathan@wholm.com>
2022-04-20 11:02:31 -0700

    chore: improved documentation

--- api/src/environment.d.ts
+++ api/src/environment.d.ts
@@ -3,13 +3,13 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production';
-      PORT?: number;
+      PORT?: number; // Coerce to number before use
       ACCESS_TOKEN_SECRET: string;
       REFRESH_TOKEN_SECRET: string;
-      CORS_ORIGIN_CLIENTS: string; // A JSON array of strings, must be split by "," when used.
+      CORS_ORIGIN_CLIENTS: string; // A JSON array of strings, must be split by "," before use.
       TYPEORM_CONNECTION: string;
       TYPEORM_HOST: string;
-      TYPEORM_PORT: number;
+      TYPEORM_PORT: number; // Coerce to number before use
       TYPEORM_USERNAME: string;
       TYPEORM_PASSWORD: string;
       TYPEORM_DATABASE: string;
@@ -18,8 +18,12 @@ declare global {
       TYPEORM_MIGRATIONS_DIR: string;
       TYPEORM_SUBSCRIBERS_DIR: string;
       TYPEORM_ENTITIES_DIR: string;
+      TYPEORM_MIGRATIONS: string;
+      TYPEORM_SUBSCRIBERS: string;
+      TYPEORM_ENTITIES: string;
+      SALT_ROUNDS: number; // Coerce to number before use
     }
   }
 }
 
-export default ProcessEnv;
+export { };
