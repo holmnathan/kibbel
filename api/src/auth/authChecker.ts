5b41239d5284265017f15d8ecacb2f776f538ee2
Nathan Holm <nathan@wholm.com>
2022-05-12 21:02:12 -0700

    refactor: user context validation
    refactor: move main user validation logic to Context.ts

--- api/src/auth/authChecker.ts
+++ api/src/auth/authChecker.ts
@@ -1,16 +1,8 @@
-import { getBearerToken, verifyToken } from '@kibbel/auth';
-import type Context from '@kibbel/Context';
+import type { IContext } from '@kibbel/Context';
 import type { AuthChecker } from 'type-graphql';
 
-const authChecker: AuthChecker<Context> = ({ context: { request } }) => {
-  // Verify request contains an “Authorization” header
-  const { authorization } = request.headers;
-  const token = getBearerToken(authorization);
-  if (!token) return false;
-
-  // Verify “Authorization” header is from a valid user
-  return !!verifyToken({ token, tokenType: 'ACCESS' });
+const authChecker: AuthChecker<IContext> = ({ context: { user } }) => {
+  return !!user;
 };
 
-export default authChecker;
 export { authChecker };
