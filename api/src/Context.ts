be14a309b173241ba4e2dc76ce4e9cecf50f29af
Nathan Holm <nathan@wholm.com>
2022-05-12 21:06:30 -0700

    feat: add user context validation
    style: Context interface renamed to IContext for convention
    feat: add userContext to validate an access token from fastify request
    feat: save user payload to context

--- api/src/Context.ts
+++ api/src/Context.ts
@@ -1,10 +1,41 @@
+import { User } from '@kibbel/entities';
+import { AuthenticationError } from 'apollo-server-fastify';
 import type { FastifyReply, FastifyRequest } from 'fastify';
-import type { User } from '@kibbel/entities';
+import { AccessToken, getBearerToken } from './auth';
 
-interface Context {
+interface IContext {
   request: FastifyRequest;
   reply: FastifyReply;
-  payload?: User;
+  user?: User;
+  tokenPayload?: AccessToken;
 }
 
-export default Context;
+const userContext = async ({ request, reply }: IContext): Promise<IContext> => {
+  let context: IContext = { request, reply };
+  try {
+    // Verify request contains an “Authorization” header
+    const { authorization } = request.headers;
+    if (!authorization) return context;
+
+    const token = getBearerToken(authorization);
+    if (!token)
+      throw new AuthenticationError(
+        'Authorization bearer token missing or invalid'
+      );
+
+    // Verify “Authorization” header is from a valid user
+    const payload = await AccessToken.verify(token);
+    if (!payload) throw new AuthenticationError('Token is invalid or corrupt');
+    const { sub: id } = payload;
+    const user = await User.findOneOrFail(id);
+    context = { ...context, user, tokenPayload: payload };
+    reply.log.info(`CONTEXT: Authorized user “${user.email}”`);
+  } catch (error) {
+    if (error instanceof AuthenticationError)
+      reply.log.warn(`CONTEXT: ${error}`);
+  } finally {
+    return context;
+  }
+};
+
+export { IContext, userContext };
