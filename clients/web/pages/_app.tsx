13d87b8a5b29dd9bccf254f38e268f55908d4753
Nathan Holm <nathan@wholm.com>
2022-06-29 17:14:19 -0700

    refactor: get ApolloProvider client from useApollo
    refactor: rename Layout component to PageLayout to clarify intended use

--- clients/web/pages/_app.tsx
+++ clients/web/pages/_app.tsx
@@ -1,15 +1,16 @@
 // Import NPM Packages
-import "bootstrap/dist/css/bootstrap.css";
-import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
-import { useEffect } from "react";
-
+import { PageLayout } from "@kibbel/components";
+import { useApollo } from "@kibbel/library/apollo";
 // Import Local Modules
 import "@kibbel/styles/globals.css";
-import { client } from "@kibbel/library/apollo";
-import { Layout } from "@kibbel/components";
+import "bootstrap/dist/css/bootstrap.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
   useEffect(() => {
     typeof document !== undefined
       ? require("bootstrap/dist/js/bootstrap")
@@ -17,10 +18,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <ApolloProvider client={client}>
-      <Layout>
+    <ApolloProvider client={apolloClient}>
+      <PageLayout>
         <Component {...pageProps} />
-      </Layout>
+      </PageLayout>
     </ApolloProvider>
   );
 }
